b36ec8d1e825157e5d7e0514aa5c4b1d67a3771d
anikap22 <anika.petach@gmail.com>
2018-03-04 17:56:29 -0500

    changes from code review

--- seqlib/__init__.py
+++ seqlib/__init__.py
@@ -1,3 +1,4 @@
-init = """
-from .seqlib import Seqlib
-"""
+#!/usr/bin/env python
+
+## from the seqlib.py file in the seqlib dir import the Seqlib class
+from seqlib.seqlib import Seqlib
--- seqlib/seqlib.py
+++ seqlib/seqlib.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+"""
+seqlib library for class assignment
+"""
+
+import copy
 import numpy as np
 import pandas as pd
 
@@ -7,16 +14,19 @@ class Seqlib:
     def __init__(self, ninds, nsites):
         self.ninds = ninds
         self.nsites = nsites
-        self.seqs = self.simulate()
+        self.seqs = self._simulate()
         # ...
 
-    def mutate(self, base):
+        ## store maf of the full seq array
+        self.maf = self._get_maf()
+
+    def _mutate(self, base):
         diff = set("ACTG") - set(base)
         return np.random.choice(list(diff))
 
     # define simulate function to simulate sequences and mutations
     # define simulate function to simulate sequences and mutations
-    def simulate(self):
+    def _simulate(self):
         ninds = self.ninds
         nsites = self.nsites
         # create initial random sequence of length nsites
@@ -36,52 +46,84 @@ class Seqlib:
         arr[missing.astype(bool)] = "N"
         return arr
 
-    # define function to remove columns with a missing value
-    def filter_missing(self, maxfreq):
-        arr = self.simulate()
-        # calculate the fraction of rows with a missing base at that site
-        freqmissing = np.sum(arr == "N", axis=0) / arr.shape[0]
-        # return the array excluding the columns that had more missing than thr
-        return arr[:, freqmissing <= maxfreq]
+    def _get_maf(self):
+        "returns the maf of the full seqarray while not counting Ns"
+        ## init an array to fill and iterate over columns
+        maf = np.zeros(self.nsites)
+        for col in range(self.nsites):
+            ## select this column of bases
+            thiscol = self.seqs[:, col]
 
-    # define function that takes array of sequences and its mutations through t
-    def filter_maf(self, arr, minfreq):
-        # arr = self.simulate()
-        # calculates mutation freq (number of rows in a column that are not
-        # equal to the first row over column length)
-        freqs = np.sum(arr != arr[0], axis=0) / arr.shape[0]
-        # make a copy of the frequency sequence
-        maf = freqs.copy()
-        # select sites where the freq is over .5 and replace with 1-that value
-        maf[maf > 0.5] = 1 - maf[maf > 0.5]
-        # return the array with all rows and columns where the maf value is
-        return arr[:, maf > minfreq]
+            ## mask "N" bases and get new length
+            nmask = thiscol != "N"
+            no_n_len = np.sum(nmask)
 
-    # return maf as floats
-    def maf(self):
-        arr = self.simulate()
-        # calculates mutation freq
-        freqs = np.sum(arr != arr[0], axis=0) / arr.shape[0]
-        # make a copy of the frequency sequence
-        maf = freqs.copy()
-        # select sites where the freq is over .5 and replace with 1-that value
-        maf[maf > 0.5] = 1 - maf[maf > 0.5]
-        return freqs
+            ## mask "N" bases and get the first base
+            first_non_n_base = thiscol[nmask][0]
 
-    # return filtered by missing and minfreq
-    def filter(self, minfreq, maxfreq):
-        maf = self.filter_maf(self.filter_missing(maxfreq), minfreq)
+            ## calculate maf of "N" masked bases
+            freq = np.sum(thiscol[nmask] != first_non_n_base) / no_n_len
+            if freq > 0.5:
+                maf[col] = 1 - freq
+            else:
+                maf[col] = freq
         return maf
 
+    # define function to remove columns with a missing value
+    def _filter_missing(self, maxmissing):
+        # calculate the fraction of rows with a missing base at that site
+        freqmissing = np.sum(self.seqs == "N", axis=0) / self.seqs.shape[0]
+        return freqmissing > maxmissing
+
+    # define function that takes array of sequences and its mutations through t
+    def _filter_maf(self, minmaf):
+        return self.maf < minmaf
+
+    # return filtered by missing and minfreq
+    def filter(self, minmaf, maxmissing):
+        filter1 = self._filter_maf(minmaf)
+        filter2 = self._filter_missing(maxmissing)
+        fullfilter = filter1 + filter2
+        return self.seqs[:, np.invert(fullfilter)]
+
+    def filter_seqlib(self, minmaf, maxmissing):
+        """
+        Applies maf and missing filters to the array and returns a copy 
+        of the seqlib object where the .seqs array has been filtered
+
+        Parameters
+        ----------
+        minmaf: float
+            The minimum minor allele frequency. Filter columns below this.
+        maxmissing: float
+            The maximum prop. missing data. Filter columns with prop Ns > this.
+        """
+        ## apply filters to get new array size
+        newseqs = self.filter(minmaf, maxmissing)
+
+        ## make a new copy of the seqlib object
+        newself = copy.deepcopy(self)       
+        newself.__init__(newseqs.shape[0], newseqs.shape[1]) 
+
+        ## store the array (overwrite it)
+        newself.seqs = newseqs
+
+        ## call the _get_maf to match new array
+        newself._get_maf()
+        return newself
+
     def calculate_statistics(self):
-        arr = self.simulate()
-        nd = np.var(arr == arr[0], axis=0).mean()
-        mf = np.mean(np.sum(arr != arr[0], axis=0) / arr.shape[0])
-        inv = np.any(arr != arr[0], axis=0).sum()
-        var = arr.shape[1] - inv
-        return pd.Series(
-            {"mean nucleotide diversity": nd,
-             "mean minor allele frequency": mf,
-             "invariant sites": inv,
-             "variable sites": var,
-             })
+        if self.seqs.size:
+            nd = np.var(self.seqs == self.seqs[0], axis=0).mean()
+            mf = np.mean(
+                np.sum(self.seqs != self.seqs[0], axis=0) / self.seqs.shape[0])
+            inv = np.all(self.seqs == self.seqs[0], axis=0).sum()
+            var = self.seqs.shape[1] - inv
+            return pd.Series(
+                {"mean nucleotide diversity": nd,
+                 "mean minor allele frequency": mf,
+                 "invariant sites": inv,
+                 "variable sites": var,
+                })
+        else:
+            print("seqs array is empty")
